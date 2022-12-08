b34c4eb9fe1adff6e3253d9d1f35b2832e2e378d
JacksonMuriithi <jacksonmuriithi299@mail.com>
2022-12-08 04:56:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution here!
+// Write your solution 
+const cats = ["Milo", "Otis","Garfield"];
+function logcats(cats){
+    console.log(cats);
+}
+function ArrayFunctions(cats){
+    function beforeEach(){
+        cats.length=0;
+        cats.push("Milo", "Otis","Garfield");
+        return(cats);
+    }
+}
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+    return(cats);
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+    return(cats);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop("Ralph")
+    return(cats);
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift("Bob")
+    return (cats);
+}
+function appendCat(name){
+    const allCats =[...cats, "Broom"]
+    return (allCats)
+}
+function prependCat(name){
+    const allCats =["Arnold", ...cats]
+    return (allCats)
+}
+function removeLastCat(){
+    const newCats = [...cats]
+    newCats.splice(-1)
+    return newCats
+
+}
+function removeFirstCat(){
+const newCats = [...cats]
+newCats.shift()
+return newCats
+}
